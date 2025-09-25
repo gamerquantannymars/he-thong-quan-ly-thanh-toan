767adbc0e2d88d9fca6c9e13bdcdbb5de3854e4b
gamerquantannymars <oshiieyov@hotmail.com>
2025-09-25 17:37:14 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hệ Thống Quản Lý Thanh Toán</h1>
+      <p>Ứng dụng thanh toán trực tuyến dễ dàng và an toàn.</p>
+    </div>
+  );
+}
+
+export default App;
